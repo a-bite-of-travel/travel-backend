19852ccd2abdbe0125d27c2162ca2456b57fe2b2
jijayoung <youhere@gmail.com>
2024-12-03 17:16:39 +0900

    fix:밸리데이션 수정

--- src/middlewares/validationErrorHandler.js
+++ src/middlewares/validationErrorHandler.js
@@ -3,13 +3,9 @@ const { validationResult } = require('express-validator');
 const validationErrorHandler = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        const errorMessages = errors.array()
-            .map(error => `${error.param}: ${error.msg}`)
-            .join(', ');
-
         return res.status(400).json({
             message: '회원가입 실패',
-            errors: errorMessages,
+            errors: errors.array(), // 배열로 반환
         });
     }
     next();
