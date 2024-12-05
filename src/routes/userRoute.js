ad8bd72183a34a502833d927b59c70666dd4daa5
jijayoung <youhere@gmail.com>
2024-12-05 11:36:18 +0900

    fix:회원가입 이미지업로드 일부수정

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,10 +3,11 @@ const { register, deactivateUser } = require('../controllers/userController');
 const { validationErrorHandler } = require('../middlewares/validationErrorHandler');
 const { registerValidator } = require('../utils/registerValidator');
 const { authenticate } = require('../middlewares/authenticate');
+const upload = require('../middlewares/upload.js'); // 파일 업로드 미들웨어 추가
 
 const router = express.Router();
 
-router.post('/register', registerValidator, validationErrorHandler, register);
+router.post('/register', upload.single('profileImage'), registerValidator, validationErrorHandler, register);
 router.delete('/', authenticate, deactivateUser);
 
 module.exports = router;
